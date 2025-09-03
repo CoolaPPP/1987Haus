bf13ecce15809cef1f74d33cfaa5470fe030717c
CoolaPPP <coola2.2544@gmail.com>
2025-09-04 01:35:54 +0700

    edit-gitignore

---
+++ resources/views/layouts/partials/info.blade.php
@@ -0,0 +1,10 @@
+<!-- Copyright © 2025. Department of Computer, CMRU. All rights reserved. -->
+
+<!-- เว็บไซต์นี้มีไว้เพื่อการศึกษาของนักศึกษา ในรายวิชาการศึกษาอิสระเท่านั้น -->
+<!-- This website is intended for educational purposes only for students pursuing an Independent Study courses. -->
+
+<!-- จัดทำโดย นาย เผ่าพันธ์ โพธิ์ธรรม รหัสนักศึกษา 65143020 -->
+<!-- Developed by Phaophan Photham Student Id : 65143020 -->
+
+<!-- มหาวิทยาลัยราชภัฏเชียงใหม่ -->
+<!-- Chiang Mai Rajabhat University -->
